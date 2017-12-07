a0582c2f3f72421dd05a6d93801b4f607325ff2a
svudya <vudhyasaideepak@gmail.com>
2017-12-07 17:31:06 +0530

    Changed some css classes to IDs

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,7 +3,7 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    const info = { id: 11, username: 'deepak', role: 'member' };
+    const info = { id: 11, username: 'deepak', role: 'Admin' };
     const signin = { email: '', password: '' };
     const forgotpassword = { email: '' };
     return { info, signin, forgotpassword };
