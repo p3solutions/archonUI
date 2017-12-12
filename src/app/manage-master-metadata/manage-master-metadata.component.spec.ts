d0a64be5ff010d7f2564cb3dfa3352be68ad889f
chandruashwin <chandruashwin22@gmail.com>
2017-12-12 10:28:02 +0530

    Added Testcase for GET method,and tested the total count of available data in manage_master_metadata

--- src/app/manage-master-metadata/manage-master-metadata.component.spec.ts
+++ src/app/manage-master-metadata/manage-master-metadata.component.spec.ts
@@ -1,13 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ManageMasterMetadataComponent } from './manage-master-metadata.component';
-
+import { Manage_Master_Metadata } from '../master-metadata-data';
+import { ManageMasterMetadataService } from '../manage-master-metadata.service';
 describe('ManageMasterMetadataComponent', () => {
   let component: ManageMasterMetadataComponent;
   let fixture: ComponentFixture<ManageMasterMetadataComponent>;
-
+  let manage_Master_Metata : Manage_Master_Metadata[];
+  let obj : Manage_Master_Metadata;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ManageMasterMetadataService],
       declarations: [ ManageMasterMetadataComponent ]
     })
     .compileComponents();
@@ -19,7 +23,18 @@ describe('ManageMasterMetadataComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('getManageMemberMetadataDetails()', () => {
+    it('should return an Observable<Array<ManageMemberMetadataDetail>>',
+      inject([ManageMasterMetadataService], (manageMasterMetadataService) => {
+        manageMasterMetadataService.getManageMasterMetaData().subscribe(manage_Master_Metata => {
+          console.log("The total length is : "+manage_Master_Metata.length);
+          obj = manage_Master_Metata[0];
+         // console.log(obj.description+" "+obj.createdDate+" "+obj.slNo+" "+obj.version);
+          expect(manage_Master_Metata.length).toBe(5);
+         
+        });
+      }));
   });
+
+ 
 });
