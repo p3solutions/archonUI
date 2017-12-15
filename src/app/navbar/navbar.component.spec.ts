1940e42dfc354de1cb84a03f50936a020eb1f2a1
Shammi Hans <shammihans1@gmail.com>
2017-12-15 17:12:52 +0530

    added a id for notification & 5 test cases for Navbar for both Admin & member

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -5,6 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NavbarComponent } from './navbar.component';
 import { InfoService } from '../info.service';
+import { Info } from '../info';
+import { Observable } from 'rxjs/Observable';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -12,13 +14,20 @@ describe('NavbarComponent', () => {
   let de: DebugElement;
   let navbarTag: HTMLElement;
   let infoService: any;
-  let infoServiceStub: any;
+  // mock data
+  const info: any = { id: 11, username: 'deepak', role: 'Member' };
+  const simpleObservable = new Observable<Info>((observer) => {
+    // observable execution
+    observer.next(info);
+    observer.complete();
+  });
+  const getInfo = function (): Observable<Info> {
+    // subscribe to the observable
+    simpleObservable.subscribe();
+    return simpleObservable;
+  };
 
   beforeEach(async(() => {
-    infoServiceStub =  { // mock-response-data to be used.
-      info : {role: 'Admin'}
-    };
-
     TestBed.configureTestingModule({
       imports: [
         HttpClientModule,
@@ -26,21 +35,25 @@ describe('NavbarComponent', () => {
       ],
       declarations: [ NavbarComponent ],
       providers: [
-        { provide: InfoService, useValue: infoServiceStub },
+        InfoService,
         HttpClientModule
       ],
     })
     .compileComponents();
-    console.log('async');
   }));
 
   beforeEach(() => {
+    // create component and test fixture
     fixture = TestBed.createComponent(NavbarComponent);
+
+    // get test component from the fixture
     component = fixture.componentInstance;
+
+    // InfoService provided to the TestBed
+    infoService = TestBed.get(InfoService);
+
     de = fixture.debugElement.query(By.css('nav'));
-    infoService = de.injector.get(InfoService);
     navbarTag = de.nativeElement;
-    console.log('sync');
   });
 
   it('Should create Navbar tag', () => {
@@ -48,14 +61,46 @@ describe('NavbarComponent', () => {
   });
 
   it('Should have the default selected Home menu in Navbar', () => {
-    const selectedMenu: HTMLElement = navbarTag.querySelector('li.active>a');
+    const selectedMenu: Element = navbarTag.querySelector('li.active>a');
     expect(selectedMenu.textContent).toContain('Home');
   });
 
-  it('Should display the username in menu by fetching', () => {
-    component.getInfo();
+  it('Should not display the username in Navbar before calling getInfo', () => {
+    const userName: Element = navbarTag.querySelector('li.user-menu>a');
+    // info.username not rendered
+    expect(userName.textContent.trim()).toBe('');
+  });
+
+  it('Should display the username in Navbar for any user', () => {
+    const userName: Element = navbarTag.querySelector('li.user-menu>a');
+    // while spying on real service, mocked info is returned
+    spyOn(infoService, 'getinfo').and.returnValue(getInfo());
     fixture.detectChanges();
-    // navbarTag.querySelector('');
-    console.log('inside Navbar tag', component, navbarTag, infoService);
+    expect(userName.textContent.trim()).toBe(component.info.username);
+    // console.log('username->', component.info.username, 'info returned by infoservice->', component.info);
+  });
+
+  it('Should display username, Manage-link, notifications in Navbar for Admin role', () => {
+    // to test for Admin, set role = 'Admin'
+    info.role = 'Admin';
+    // while spying on real service, mocked info is returned
+    spyOn(infoService, 'getinfo').and.returnValue(getInfo());
+    fixture.detectChanges();
+    // call getInfo() to set info.show true for Admin
+    component.getInfo();
+
+    // detecting changes
+    fixture.detectChanges();
+
+    // expecting username
+    const userName: Element = navbarTag.querySelector('li.user-menu>a');
+    expect(userName.textContent.trim()).toBe(component.info.username);
+    // expecting Manage link
+    const manageLink: Element = navbarTag.querySelector('li[title="Manage"]>a');
+    expect(manageLink.textContent.trim()).toBe('Manage');
+
+    const notifications: Element = navbarTag.querySelector('#see-all-notif');
+    expect(notifications.textContent.trim()).toBe('See All Notifications');
+    // console.log('info returned by infoservice->', component.info, manageLink, notifications);
   });
 });
