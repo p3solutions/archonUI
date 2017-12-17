3575324ae25c795416d90023fb4442bf1d45aec7
svudya <vudhyasaideepak@gmail.com>
2017-12-17 23:38:01 +0530

    Local changes to workspace header

---
+++ src/app/workspace-mgmt-panel/workspace-mgmt-panel.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WorkspaceMgmtPanelComponent } from './workspace-mgmt-panel.component';
+
+describe('WorkspaceMgmtPanelComponent', () => {
+  let component: WorkspaceMgmtPanelComponent;
+  let fixture: ComponentFixture<WorkspaceMgmtPanelComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ WorkspaceMgmtPanelComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WorkspaceMgmtPanelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
