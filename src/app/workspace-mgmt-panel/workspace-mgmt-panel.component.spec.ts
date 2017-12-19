8ba5cc728187602d6baadaa62c8855d2d0e07b86
Shammi Hans <shammihans1@gmail.com>
2017-12-19 22:52:14 +0530

    test cases for workspace-mgmt-panel

--- src/app/workspace-mgmt-panel/workspace-mgmt-panel.component.spec.ts
+++ src/app/workspace-mgmt-panel/workspace-mgmt-panel.component.spec.ts
@@ -1,10 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { WorkspaceMgmtPanelComponent } from './workspace-mgmt-panel.component';
+// import { HttpClientModule } from '@angular/common/http';
+// import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-xdescribe('WorkspaceMgmtPanelComponent', () => {
+describe('WorkspaceMgmtPanelComponent', () => {
   let component: WorkspaceMgmtPanelComponent;
   let fixture: ComponentFixture<WorkspaceMgmtPanelComponent>;
+  let de: DebugElement;
+  let workspaceMgmtPanel: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +21,23 @@ xdescribe('WorkspaceMgmtPanelComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(WorkspaceMgmtPanelComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('#workspace-mgmt-panel'));
+    workspaceMgmtPanel = de.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should have the tab names in the panel', () => {
+    const workspaceInfo: Element = workspaceMgmtPanel.querySelector('a.workspace-info');
+    expect(workspaceInfo.textContent).toContain('Workspace Info');
+    const workspaceName: Element = workspaceMgmtPanel.querySelector('a.member-requests');
+    expect(workspaceName.textContent).toContain('Member Requests');
+    const workspaceRole: Element = workspaceMgmtPanel.querySelector('a.manage-members');
+    expect(workspaceRole.textContent).toContain('Manage Members');
+    const workspaceMeta: Element = workspaceMgmtPanel.querySelector('a.manage-master-metadata');
+    expect(workspaceMeta.textContent).toContain('Manage Master Metadata');
   });
 });
