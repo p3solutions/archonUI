9c1945ef2d93809a3fe0eca60dcd95c00fce34be
Shammi Hans <shammihans1@gmail.com>
2017-12-26 12:34:00 +0530

    Test cases for logic of this component, task:https://app.asana.com/0/485886451522805/509740085068972/f

--- src/app/workspace-landing-page/workspace-landing-page.component.spec.ts
+++ src/app/workspace-landing-page/workspace-landing-page.component.spec.ts
@@ -1,35 +1,72 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { WorkspaceLandingPageComponent } from './workspace-landing-page.component';
 import { NavbarComponent } from '../navbar/navbar.component';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { Router } from '@angular/router';
-// import { routes } from '../app-routing.module';
-// import { Location } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { InfoService } from '../info.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Observable } from 'rxjs/Observable';
+import { Info } from '../info';
 
-xdescribe('WorkspaceLandingPageComponent', () => {
+describe('WorkspaceLandingPageComponent', () => {
   let component: WorkspaceLandingPageComponent;
   let fixture: ComponentFixture<WorkspaceLandingPageComponent>;
-  // let location: Location;
-  // let router: Router;
+  let infoService: any;
+
+  // mock data
+  const info: any = { id: 11, username: 'deepak', role: 'Member' };
+  const simpleObservable = new Observable<Info>((observer) => {
+    observer.next(info);
+    observer.complete();
+  });
+  let disposeMe;
+  const getInfo = function (): Observable<Info> {
+    disposeMe = simpleObservable.subscribe();
+    return simpleObservable;
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      // imports: [RouterTestingModule.withRoutes(routes)],
-      declarations: [ WorkspaceLandingPageComponent, NavbarComponent ]
+      imports: [RouterTestingModule, HttpClientModule, HttpClientTestingModule],
+      providers: [RouterTestingModule, InfoService, HttpClientTestingModule ],
+      declarations: [WorkspaceLandingPageComponent, NavbarComponent]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    // router = TestBed.get(Router);
-    // location = TestBed.get(Location);
     fixture = TestBed.createComponent(WorkspaceLandingPageComponent);
     component = fixture.componentInstance;
-    // router.initialNavigation();
+    infoService = TestBed.get(InfoService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should get data from InfoService', () => {
+    spyOn(infoService, 'getinfo').and.returnValue(getInfo());
+    component.getInfo();
+    fixture.detectChanges();
+    expect(info.username).toBe(component.info.username);
+    disposeMe.unsubscribe();
+  });
+
+  it('Should have show == undefined for Member', () => {
+    spyOn(infoService, 'getinfo').and.returnValue(getInfo());
+    component.getInfo();
+    fixture.detectChanges();
+    expect(component.info.show).toBeUndefined(); // or may be toBeFalsy
+    disposeMe.unsubscribe();
+  });
+
+  it('Should have show == true for Admin', () => {
+    info.role = 'Admin';
+    spyOn(infoService, 'getinfo').and.returnValue(getInfo());
+    component.getInfo();
+    fixture.detectChanges();
+    expect(component.info.show).toBeTruthy();
+    disposeMe.unsubscribe();
   });
 });
