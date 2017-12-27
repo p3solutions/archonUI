3dfed73315ed5363e721be9f90d100e5992d22ab
Shammi Hans <shammihans1@gmail.com>
2017-12-27 10:56:12 +0530

    test-case for gotoDashboard(), however test-cases of this component is not running, https://app.asana.com/0/485886451522805/509740085068972/f

--- src/app/workspace-info/workspace-info.component.spec.ts
+++ src/app/workspace-info/workspace-info.component.spec.ts
@@ -1,22 +1,22 @@
 import { Workspaceinfo } from '../workspaceinfo';
 import { WorkspaceInfoComponent } from './workspace-info.component';
 import { WorkspaceinfoService } from '../workspaceinfo.service';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Observable } from 'rxjs/Observable';
+import { WorkspaceServicesComponent } from '../workspace-services/workspace-services.component';
 
 xdescribe('WorkspaceInfoComponent', () => {
   let component: WorkspaceInfoComponent;
   let fixture: ComponentFixture<WorkspaceInfoComponent>;
-  // tslint:disable-next-line:prefer-const
-  let workInfoData: Workspaceinfo;
   let de: DebugElement;
   let WorkspaceInfoTag: HTMLElement;
   let workspaceinfoService: any;
+  let workspaceServiceTag: any;
+  const dashboardUrl = 'workspace/workspace-dashboard/workspace-services';
   const managemembers1: any = {
     name: 'Frontend Developer', owner: 'Platform3Solutions', approver: 'User1, User2',
     members: 'User1, User2, User3', your_role: 'Admin', master_metadata_version: '22'
@@ -36,17 +36,16 @@ xdescribe('WorkspaceInfoComponent', () => {
 
     TestBed.configureTestingModule({
       imports: [
-        HttpClientModule,
-        HttpClientTestingModule
+        HttpClientTestingModule,
+        RouterTestingModule
       ],
       declarations: [WorkspaceInfoComponent],
       providers: [
-        WorkspaceinfoService,
-        HttpClientModule
+        RouterTestingModule,
+        WorkspaceinfoService
       ],
     })
       .compileComponents();
-    console.log('changed');
   }));
 
   beforeEach(() => {
@@ -55,37 +54,45 @@ xdescribe('WorkspaceInfoComponent', () => {
     de = fixture.debugElement.query(By.css('#workspace-info-table'));
     WorkspaceInfoTag = de.nativeElement;
     workspaceinfoService = TestBed.get(WorkspaceinfoService);
+    fixture.detectChanges();
   });
 
   it('Should create Workspace_Info tag', () => {
+    // console.log(component, 'component');
     expect(component).toBeTruthy();
   });
 
-  it('Should display the observable data for workspace-info componenet', () => {
+  xit('Should display the observable data for workspace-info componenet', () => {
     const row1Array: NodeListOf<Element> = WorkspaceInfoTag.querySelectorAll('.ws-info-data');
-    // while spying on real service, mocked info is returned
     spyOn(workspaceinfoService, 'getworkinfo').and.returnValue(getworkinfo());
     fixture.detectChanges();
-    console.log(row1Array, component.workspaceInfoData);
+    console.log(row1Array, 'row1Array');
     const name = row1Array[0];
     const owner = row1Array[1];
     const approver = row1Array[2];
     const member = row1Array[3];
     const role = row1Array[4];
     const metadata_version = row1Array[5];
-    // console.log('alok', name);
-    // console.log('alok', owner);
-    // console.log('alok', approver);
-    // console.log('alok', member);
-    // console.log('alok', role);
-    // console.log('alok', metadata_version);
     expect(name.textContent.trim()).toBe(component.workspaceInfoData.name);
     expect(owner.textContent.trim()).toBe(component.workspaceInfoData.owner);
     expect(approver.textContent.trim()).toBe(component.workspaceInfoData.approver);
     expect(member.textContent.trim()).toBe(component.workspaceInfoData.members);
     expect(role.textContent.trim()).toBe(component.workspaceInfoData.your_role);
     expect(metadata_version.textContent.trim()).toBe(component.workspaceInfoData.master_metadata_version);
-
+    disposeMe.unsubscribe();
   });
-
+  // ToDo: revisit again
+  xit('Should navigate to dashboard', () => {
+    component.gotoDashboard();
+    fixture.detectChanges();
+    // find DebugElements with an attached WorkspaceServicesComponentDirective
+    workspaceServiceTag = fixture.debugElement
+      .queryAll(By.directive(WorkspaceServicesComponent));
+    // console.log('workspaceServiceTag', workspaceServiceTag);
+    // get the attached link directive instances using the DebugElement injectors
+    const links = workspaceServiceTag
+      .map(dE => dE.injector.get(WorkspaceServicesComponent) as WorkspaceServicesComponent);
+    // console.log('links', links);
+    expect(links[1].navigatedTo).toBe(dashboardUrl);
+  });
 });
