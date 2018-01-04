dfea7642c3f0bf69022b453d2a522d7c9b9fa7ed
Alok <alokchoudhary162@gmail.com>
2018-01-04 15:47:52 +0530

    formatted

--- src/app/manage-user-roles/manage-user-roles.service.ts
+++ src/app/manage-user-roles/manage-user-roles.service.ts
@@ -14,38 +14,24 @@ import { Data } from '@angular/router/src/config';
 @Injectable()
 export class ManageUserRolesService {
 
-  private headers = new HttpHeaders({ 'Content-Type': 'application/json',
-   'Authorization': 'Bearer ' + localStorage.getItem('accessToken') });
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + localStorage.getItem('accessToken')
+  });
   private globalRolesUrl = 'http://13.58.89.64:9000/users';
   // manageUserRolesUrl = 'api/manage_user_roles';
   constructor(private http: HttpClient) { }
 
-  // getManageMembersDetails() {
-  //   this.http.get(this.globalRolesUrl, {headers: this.headers})
-  //   .subscribe(
-  //     (data: any[]) => {
-  //         console.log('hiiiii', data['data']['users']);
-  //     }
-  //   );
-  // }
-//   [04/01/2018 12:43 PM] Shammi Hans: getData():Observable<Post[]> {
-//     return this.http.get('http://jsonplaceholder.typicode.com/posts/')
-//         .map(this.extractData)
-//         .catch(this.handleError);
-// }
-
-private extractData(res: any) {
+  private extractData(res: any) {
     const body = res.data.users;
     return body || [];
-}
-
+  }
 
   getManageMembersDetails(): Observable<ManageUserRoles[]> {
-    return this.http.get<ManageUserRoles[]>(this.globalRolesUrl, {headers: this.headers}).map(this.extractData).pipe(
+    return this.http.get<ManageUserRoles[]>(this.globalRolesUrl, { headers: this.headers }).map(this.extractData).pipe(
       catchError(this.handleError('manageuserroles', []))
     );
   }
-
 
   // * Handle Http operation that failed.
   // * Let the app continue.
