d57e951534e693b9afd1123f1ff3c638b7042180
Alok <alokchoudhary162@gmail.com>
2018-01-11 15:47:32 +0530

    integrated backened api  https://app.asana.com/0/0/513696203165943/f

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ChangePasswordService } from './change-password.service';
+import { ErrorObject } from '../error-object';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ChangePassword } from '../ChangePassword';
+import { HttpErrorResponse } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-change-password',
@@ -6,10 +12,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent implements OnInit {
-
-  constructor() { }
+  changepassword: ChangePassword;
+  changePasswordForm: FormGroup;
+  errorObject: ErrorObject;
+  oldPassword: string;
+  newPassword: string;
+  confirmPassword: string;
+  @Input() userId: string;
+  constructor(private changePasswordService: ChangePasswordService) { }
 
   ngOnInit() {
+    this.createForm();
   }
+  createForm() {
+    this.changePasswordForm = new FormGroup({
+      oldPassword: new FormControl('', [Validators.required]),
+      newPassword: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [Validators.required])
+    });
+  }
+
+  isPasswordNotValidated() {
+    this.newPassword = this.changePasswordForm.value.newPassword;
+    this.confirmPassword = this.changePasswordForm.value.confirmPassword;
+    if (this.newPassword.length < 6) {
+      this.errorObject = new ErrorObject;
+      this.errorObject.message = 'Password length must be atleast 6 character';
+      this.errorObject.show = true;
+    }
+    if (this.newPassword !== this.confirmPassword) {
+      this.errorObject = new ErrorObject;
+      this.errorObject.message = 'New Password and Confirm Password mismatched';
+      this.errorObject.show = true;
+    }
+    return this.errorObject;
+  }
+  changeUserPassword() {
+    if (this.isPasswordNotValidated()) {
+      return false;
+    }
+    const param = {
+      userId: this.userId,
+      newPassword: this.newPassword,
+      oldPassword: this.changePasswordForm.value.oldPassword
+    };
+
+    this.changePasswordService.changePassword(param).subscribe((res) => {
+      (<HTMLButtonElement>document.querySelector('#changePasswordModal .cancel')).click();
+
+      console.log(res, 'changeUserPassword');
+    },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          // A client-side or network error occurred. Handle it accordingly.
+          console.log('An error occurred:', err.error.message);
+        } else {
+          // The backend returned an unsuccessful response code.
+          // The response body may contain clues as to what went wrong,
+          this.errorObject = new ErrorObject;
+          this.errorObject.message = err.error.message;
+          this.errorObject.show = !err.error.success;
+          console.log(`Backend returned code ${err.status}, body was: ${JSON.stringify(err.error)}`);
+        }
+      });
+  }
+
 
 }
--- src/app/change-password/change-password.service.ts
+++ src/app/change-password/change-password.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { ChangePassword } from '../ChangePassword';
+import { error } from 'util';
+import { catchError } from 'rxjs/operators/catchError';
+import { of } from 'rxjs/observable/of';
 
 @Injectable()
 export class ChangePasswordService {
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + localStorage.getItem('accessToken')
+  });
+  private getUsersUrl = 'http://13.58.89.64:9000/users/5a561bb4591221242dc41434/pwd';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  changePassword(param) {
+    console.log('service param', param);
+    return this.http.patch(this.getUsersUrl, param, { headers: this.headers } ).pipe(
+      catchError(this.handleError('changePassword'))
+    );
+  }
+
+   // * Handle Http operation that failed.
+  // * Let the app continue.
+  // * @param operation - name of the operation that failed
+  // * @param result - optional value to return as the observable result
+  // */
+  private handleError<T>(operation = 'operation', result?: T) {
+    // tslint:disable-next-line:no-shadowed-variable
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      return of(result as T);
+    };
+  }
+
+  /** Log a message with the MessageService */
+  private log(message: string) {
+    console.log(message);
+  }
 
 }
