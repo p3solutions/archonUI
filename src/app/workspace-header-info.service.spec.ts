e699870a270fb38786d34833bafb84ede9e669c6
chandruashwin <chandruashwin22@gmail.com>
2018-02-26 13:54:09 +0530

    Removed back arrow button on ws-service

---
+++ src/app/workspace-header-info.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { WorkspaceHeaderInfoService } from './workspace-header-info.service';
+
+describe('WorkspaceHeaderInfoService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [WorkspaceHeaderInfoService]
+    });
+  });
+
+  it('should be created', inject([WorkspaceHeaderInfoService], (service: WorkspaceHeaderInfoService) => {
+    expect(service).toBeTruthy();
+  }));
+});
