5a26480500c95b8ea2644cb28e9a651dd6f766a8
Shammi Hans <shammihans1@gmail.com>
2018-05-18 19:08:27 +0530

    reverse pipe to get array in reverse order

---
+++ src/app/reverse.pipe.ts
@@ -0,0 +1,14 @@
+import { PipeTransform, Pipe } from '@angular/core';
+// returns an array of keys of the given Object
+@Pipe({ name: 'reverseArray' })
+export class ReverseArrayPipe implements PipeTransform {
+    transform(srcArray, args: any[]): any {
+        console.log(srcArray, 'srcArr');
+        const reversedArray = [];
+        for (let i = srcArray.length; i > 0; i--) {
+            reversedArray.push(srcArray[i - 1]);
+        }
+        console.log(reversedArray, 'rev');
+        return reversedArray;
+    }
+}
