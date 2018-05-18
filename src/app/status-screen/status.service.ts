a106ceacb5d8d6f0728af22010583b957e0e20c4
Shammi Hans <shammihans1@gmail.com>
2018-05-18 19:10:13 +0530

    status service created & data brought through API

---
+++ src/app/status-screen/status.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { UserinfoService } from '../userinfo.service';
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
+
+@Injectable()
+export class StatusService {
+
+  getJobOriginsUrl = environment.apiUrl + 'jobStatus/jobOrigins';
+  getJobStatusesUrl = environment.apiUrl + 'jobStatus/jobStatuses';
+
+  constructor(
+    private http: HttpClient,
+    private userinfoService: UserinfoService
+  ) { }
+
+  getHeaders() {
+    return this.userinfoService.getHeaders();
+  }
+
+  getJobOrigins(): Observable<any> {
+    return this.http.get<any>(this.getJobOriginsUrl, { headers: this.getHeaders() })
+      .map(this.extractJobOrigins)
+      .pipe(catchError(this.handleError<any>('getUserInfo')));
+  }
+
+  getJobStatuses(): Observable<any> {
+    return this.http.get<any>(this.getJobStatusesUrl, { headers: this.getHeaders() })
+      .map(this.extractJobStatuses)
+      .pipe(catchError(this.handleError<any>('getUserInfo')));
+  }
+
+  private extractJobOrigins(res: any) {
+    const data = res.data.job_origins;
+    return data || [];
+  }
+  private extractJobStatuses(res: any) {
+    const data = res.data.job_statuses;
+    return data || [];
+  }
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      return of(result as T);
+    };
+  }
+}
