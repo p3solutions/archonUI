36ee19447ac8fd263a25740aac95725f704d14ff
Shammi Hans <shammihans1@gmail.com>
2018-05-22 14:45:49 +0530

    replaced doouble quotes with single quotes

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -73,172 +73,172 @@ export class InMemoryDataService implements InMemoryDbService {
       { slNo: '5', version: '4.69', description: 'Null', createdDate: '20/11/2017 04.05 PM' }
     ];
     const workspaceListInfo = {
-      "data": {
-        "workspaces": [
+      'data': {
+        'workspaces': [
           {
-            id: "5a55c85d9d226c08140a90d6",
+            id: '5a55c85d9d226c08140a90d6',
             createdAt: 1515571293,
             updatedAt: 1515578378,
-            workspaceName: "Workspace 1",
+            workspaceName: 'Workspace 1',
             owner: {
-              "id": "5a535cbb9d226c1a4c946607",
-              "name": "alok"
+              'id': '5a535cbb9d226c1a4c946607',
+              'name': 'alok'
             },
             masterMetadataVersion: 22,
             databases: [{
-              "id": "5a533baec7b4d489ed715b85",
-              "name": "SQL_DB_NAME",
-              "type": "SQL"
+              'id': '5a533baec7b4d489ed715b85',
+              'name': 'SQL_DB_NAME',
+              'type': 'SQL'
             },
             {
-              "id": "5a533baec7b4d489ed715b85",
-              "name": "DB2_DB_NAME",
-              "type": "DB2"
+              'id': '5a533baec7b4d489ed715b85',
+              'name': 'DB2_DB_NAME',
+              'type': 'DB2'
             }],
             members: [
               {
-                "createdAt": 1515571475,
-                "updatedAt": 1515578378,
-                "user": {
-                  "id": "5a535c4a9d226c1a4c946605",
-                  "name": "omji1"
+                'createdAt': 1515571475,
+                'updatedAt': 1515578378,
+                'user': {
+                  'id': '5a535c4a9d226c1a4c946605',
+                  'name': 'omji1'
                 },
-                "role": {
-                  "id": "5a55c819c7b4d407523007bc",
-                  "name": "ROLE_MEMBER"
+                'role': {
+                  'id': '5a55c819c7b4d407523007bc',
+                  'name': 'ROLE_MEMBER'
                 }
               }
             ],
-            "lastUpdatedTime": 'Jan 1st 2018 22:30:40'
+            'lastUpdatedTime': 'Jan 1st 2018 22:30:40'
           },
           {
-            id: "5a55c85d9d226c08140a90d6",
+            id: '5a55c85d9d226c08140a90d6',
             createdAt: 1515571293,
             updatedAt: 1515578378,
-            workspaceName: "Workspace 2",
+            workspaceName: 'Workspace 2',
             owner: {
-              "id": "5a535cbb9d226c1a4c946607",
-              "name": "alok"
+              'id': '5a535cbb9d226c1a4c946607',
+              'name': 'alok'
             },
             masterMetadataVersion: 22,
             databases: [{
-              "id": "5a533baec7b4d489ed715b85",
-              "name": "SQL_DB_NAME",
-              "type": "SQL"
+              'id': '5a533baec7b4d489ed715b85',
+              'name': 'SQL_DB_NAME',
+              'type': 'SQL'
             },
             {
-              "id": "5a533baec7b4d489ed715b85",
-              "name": "DB2_DB_NAME",
-              "type": "DB2"
+              'id': '5a533baec7b4d489ed715b85',
+              'name': 'DB2_DB_NAME',
+              'type': 'DB2'
             }],
             members: [
               {
-                "createdAt": 1515571475,
-                "updatedAt": 1515578378,
-                "user": {
-                  "id": "5a535c4a9d226c1a4c946605",
-                  "name": "omji1"
+                'createdAt': 1515571475,
+                'updatedAt': 1515578378,
+                'user': {
+                  'id': '5a535c4a9d226c1a4c946605',
+                  'name': 'omji1'
                 },
-                "role": {
-                  "id": "5a55c819c7b4d407523007bc",
-                  "name": "ROLE_MEMBER"
+                'role': {
+                  'id': '5a55c819c7b4d407523007bc',
+                  'name': 'ROLE_MEMBER'
                 }
               }
             ],
             lastUpdatedTime: 'Feb 1st 2018 22:30:40'
           },
           {
-            id: "5a55c85d9d226c08140a90d6",
+            id: '5a55c85d9d226c08140a90d6',
             createdAt: 1515571293,
             updatedAt: 1515578378,
-            workspaceName: "Workspace 3",
+            workspaceName: 'Workspace 3',
             owner: {
-              "id": "5a535cbb9d226c1a4c946607",
-              "name": "alok"
+              'id': '5a535cbb9d226c1a4c946607',
+              'name': 'alok'
             },
             masterMetadataVersion: 22,
             databases: [{
-              "id": "5a533baec7b4d489ed715b85",
-              "name": "SQL_DB_NAME",
-              "type": "SQL"
+              'id': '5a533baec7b4d489ed715b85',
+              'name': 'SQL_DB_NAME',
+              'type': 'SQL'
             },
             {
-              "id": "5a533baec7b4d489ed715b85",
-              "name": "DB2_DB_NAME",
-              "type": "DB2"
+              'id': '5a533baec7b4d489ed715b85',
+              'name': 'DB2_DB_NAME',
+              'type': 'DB2'
             }],
             members: [
               {
-                "createdAt": 1515571475,
-                "updatedAt": 1515578378,
-                "user": {
-                  "id": "5a535c4a9d226c1a4c946605",
-                  "name": "omji1"
+                'createdAt': 1515571475,
+                'updatedAt': 1515578378,
+                'user': {
+                  'id': '5a535c4a9d226c1a4c946605',
+                  'name': 'omji1'
                 },
-                "role": {
-                  "id": "5a55c819c7b4d407523007bc",
-                  "name": "ROLE_MEMBER"
+                'role': {
+                  'id': '5a55c819c7b4d407523007bc',
+                  'name': 'ROLE_MEMBER'
                 }
               }
             ],
             lastUpdatedTime: 'Jan 1st 2018 22:30:40'
           },
           {
-            id: "5a55c85d9d226c08140a90d6",
+            id: '5a55c85d9d226c08140a90d6',
             createdAt: 1515571293,
             updatedAt: 1515578378,
-            workspaceName: "Workspace 4",
+            workspaceName: 'Workspace 4',
             owner: {
-              "id": "5a535cbb9d226c1a4c946607",
-              "name": "alok"
+              'id': '5a535cbb9d226c1a4c946607',
+              'name': 'alok'
             },
             masterMetadataVersion: 22,
             databases: [{
-              "id": "5a533baec7b4d489ed715b85",
-              "name": "SQL_DB_NAME",
-              "type": "SQL"
+              'id': '5a533baec7b4d489ed715b85',
+              'name': 'SQL_DB_NAME',
+              'type': 'SQL'
             },
             {
-              "id": "5a533baec7b4d489ed715b85",
-              "name": "DB2_DB_NAME",
-              "type": "DB2"
+              'id': '5a533baec7b4d489ed715b85',
+              'name': 'DB2_DB_NAME',
+              'type': 'DB2'
             }],
             members: [
               {
-                "createdAt": 1515571475,
-                "updatedAt": 1515578378,
-                "user": {
-                  "id": "5a535c4a9d226c1a4c946605",
-                  "name": "omji1"
+                'createdAt': 1515571475,
+                'updatedAt': 1515578378,
+                'user': {
+                  'id': '5a535c4a9d226c1a4c946605',
+                  'name': 'omji1'
                 },
-                "role": {
-                  "id": "5a55c819c7b4d407523007bc",
-                  "name": "ROLE_MEMBER"
+                'role': {
+                  'id': '5a55c819c7b4d407523007bc',
+                  'name': 'ROLE_MEMBER'
                 }
               }
             ],
             lastUpdatedTime: 'Jan 1st 2018 22:30:40'
           },
           {
-            "id": "5a55dcb89d226c4607e16446",
-            "createdAt": 1515576504,
-            "updatedAt": 1515578378,
-            "workspaceName": "WS_2",
-            "masterMetadataVersion": 22,
-            "databases": [
+            'id': '5a55dcb89d226c4607e16446',
+            'createdAt': 1515576504,
+            'updatedAt': 1515578378,
+            'workspaceName': 'WS_2',
+            'masterMetadataVersion': 22,
+            'databases': [
               {
-                "id": "5a533baec7b4d489ed715b85",
-                "name": "SQL_DB_NAME",
-                "type": "SQL"
+                'id': '5a533baec7b4d489ed715b85',
+                'name': 'SQL_DB_NAME',
+                'type': 'SQL'
               }
             ],
-            "members": [],
-            "lastUpdatedTime": 'Jan 1st 2018 22:30:40'
+            'members': [],
+            'lastUpdatedTime': 'Jan 1st 2018 22:30:40'
           },
 
         ],
-        "success": true,
-        "httpStatus": 200
+        'success': true,
+        'httpStatus': 200
       }
     };
     const manage_user_roles = [
