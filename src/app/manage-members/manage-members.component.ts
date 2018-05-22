64d8a126aa93c3b65e14bfd602ccc39a78585e07
Shammi Hans <shammihans1@gmail.com>
2018-05-22 14:45:31 +0530

    space after line comment tag

--- src/app/manage-members/manage-members.component.ts
+++ src/app/manage-members/manage-members.component.ts
@@ -52,7 +52,7 @@ export class ManageMembersComponent implements OnInit {
 
   onDelete(id: any, tr): void {
     this.manageMembersService.deleteManageMembersData({ id: id }, this.workspaceId).subscribe(res => {
-      tr.remove(); //Removing the row.
+      tr.remove(); // Removing the row.
     });
   }
 
