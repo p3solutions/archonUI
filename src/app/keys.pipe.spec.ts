bbcae90d92f60718d15f23e977a5dba3870ea7b2
Shammi Hans <shammihans1@gmail.com>
2018-06-05 22:04:45 +0530

    key pipe functionality test case

--- src/app/keys.pipe.spec.ts
+++ src/app/keys.pipe.spec.ts
@@ -10,4 +10,9 @@ describe('Pipe: Keys', () => {
   it('create an KeysPipe() instance', () => {
     expect(pipe).toBeTruthy();
   });
+  it('Functionality of KeysPipe', () => {
+    const keyArray = pipe.transform({a: 1});
+    const attr = keyArray[0];
+    expect(attr).toBe('a');
+  });
 });
--- src/app/keys.pipe.ts
+++ src/app/keys.pipe.ts
@@ -2,13 +2,8 @@ import { PipeTransform, Pipe } from '@angular/core';
 // returns an array of keys of the given Object
 @Pipe({ name: 'keys' })
 export class KeysPipe implements PipeTransform {
-    transform(srcObj, args: string[]): any {
+    transform(srcObj: Object): any {
         // console.log(srcObj, 'srcObj');
-        const keys = [];
-        for (const [key, value] of Object.entries(srcObj)) {
-            keys.push(key);
-        }
-        // console.log(keys);
-        return keys;
+        return srcObj ? Object.keys(srcObj) : [];
     }
 }
