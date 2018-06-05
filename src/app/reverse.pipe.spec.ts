aaef78f147a53b7d57ac0bd60d1bb742cf8737af
Shammi Hans <shammihans1@gmail.com>
2018-06-05 22:15:41 +0530

    reverse array functionality test case

--- src/app/reverse.pipe.spec.ts
+++ src/app/reverse.pipe.spec.ts
@@ -10,4 +10,10 @@ describe('Pipe: ReverseArrayPipe', () => {
   it('create an ReverseArrayPipe() instance', () => {
     expect(pipe).toBeTruthy();
   });
+  it('Functionality of ReverseArrayPipe', () => {
+    const array = [1, 2, 3, 4, 5];
+    const reversedArray = pipe.transform(array);
+    const lastIndex = array.length - 1;
+    expect(array[0]).toBe(reversedArray[lastIndex]);
+  });
 });
