d26cc1264cf5ba79008bb92000eb5d516e8fe46c
Shammi Hans <shammihans1@gmail.com>
2018-06-05 17:52:35 +0530

    fixed path

--- src/app/workspace-header-info/workspace-header-info.service.spec.ts
+++ src/app/workspace-header-info/workspace-header-info.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { WorkspaceHeaderInfoService } from '../workspace-header-info.service';
+import { WorkspaceHeaderInfoService } from './workspace-header-info.service';
 
 describe('WorkspaceHeaderInfoService', () => {
   beforeEach(() => {
