8f4cc5b78d2f7fc02f3a74f95a6d308f2bed7947
Shammi Hans <shammihans1@gmail.com>
2018-08-11 03:20:26 +0530

    UI for data analyse step-wise process, selection of primary table then columns & selection of secondary tables then columns, final screen table UI pending

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Pipe } from '@angular/core';
 import { TableListService } from './table-list.service';
 import { RelationshipInfoObject } from '../workspace-objects';
+import { InMemoryDataService } from '../in-memory-data.service';
 @Component({
   selector: 'app-table-list',
   templateUrl: './table-list.component.html',
@@ -8,35 +9,268 @@ import { RelationshipInfoObject } from '../workspace-objects';
 })
 export class TableListComponent implements OnInit {
   public search: any = '';
-  private homeStage: boolean;
+  private homeStage = false;
   private isAvailable: boolean;
   private isRelationShipAvailable: boolean;
-  private tableName: string;
+  private selectedPrimTbl: any;
   private relationshipInfo: RelationshipInfoObject[];
   private serviceActionType: string;
+  private tableList: string[];
+  primColArray = [];
+  secColArray = [];
+  secTblArray = [];
+  selectedPrimColMap = new Map();
+  selectedSecColMap = new Map();
+  dataAModal = false;
+  analysisRowCount = 3;
+  selectedPrimCol = new Map();
+  selectedSecTbl = new Map();
+  selectedSecCol = new Map();
+  enableNextBtn = false;
+  // primColLoader = false;
+  // secColLoader = false;
   constructor(
     private tablelistService: TableListService
   ) { }
-  private tableList: string[];
+
   ngOnInit() {
-    this.homeStage = true;
     this.isAvailable = false;
     this.isRelationShipAvailable = false;
-    this.tablelistService.getTableList().subscribe(result => {
-      this.tableList = result;
+    this.getTableList();
+  }
+  getTableList() {
+    this.tablelistService.getTableList().subscribe(res => {
+      this.tableList = res;
       this.isAvailable = true;
+      // console.log(this.tableList);
     });
   }
 
-  loadRelationTable(tableName: string) {
-    this.homeStage = false;
-    this.tableName = tableName;
-    this.tablelistService.getListOfRelationTable(tableName).subscribe(result => {
-      this.relationshipInfo = result;
-      this.isRelationShipAvailable = true;
-      // console.log(this.relationshipInfo);
-    });
+  loadRelationTable(table: any) {
+    this.homeStage = true;
+    this.dataAModal = false;
+    this.selectedPrimTbl = table;
+    // this.tablelistService.getListOfRelationTable(this.selectedPrimTbl.table_name).subscribe(result => {
+    //   this.relationshipInfo = result;
+    //   this.isRelationShipAvailable = true;
+    //   // console.log(this.relationshipInfo);
+    // });
     this.serviceActionType = this.tablelistService.getServiceActionType();
   }
 
+  openDataAModal() {
+    this.homeStage = false;
+    this.dataAModal = true;
+    this.getColumnsByTableName(this.selectedPrimTbl.table_name, true);
+    this.resetDataAModal();
+  }
+  getColumnsByTableName(table_name, isPrime) {
+    if (isPrime) {
+      this.primColArray = [];
+      // this.primColLoader = true;
+    } else {
+      this.secColArray = [];
+      // this.secColLoader = true;
+    }
+    this.tablelistService.getColumnsByTableName(table_name)
+    .subscribe((columns) => {
+      if (isPrime) {
+        this.primColArray = columns;
+        // this.primColLoader = false;
+      } else {
+        this.secColArray = columns;
+        // this.secColLoader = false;
+      }
+    });
+  }
+  gotoBack() {
+    this.homeStage = true;
+    this.dataAModal = false;
+  }
+  resetDataAModal() {
+    this.enableNextBtn = false;
+  }
+
+  addToTblColMap(column, isPrimary) {
+    const tableColMap = isPrimary ? this.selectedPrimColMap : this.selectedSecColMap;
+    const secTblVal = tableColMap.get(column.table_name);
+    if (secTblVal) {
+      secTblVal.push(column);
+      console.log('isPrimary', isPrimary, 'pushed', secTblVal);
+    } else {
+      tableColMap.set(column.table_name, [column]);
+      console.log('isPrimary', isPrimary, '1st push', tableColMap);
+    }
+  }
+  removeFromTblColMap(column, isPrimary) {
+    const tableColMap = isPrimary ? this.selectedPrimColMap : this.selectedSecColMap;
+    console.log('isPrimary', isPrimary, tableColMap);
+    const secTblVal = tableColMap.get(column.table_name);
+    for (let ind = 0; ind < secTblVal.length; ind++) {
+      if (secTblVal[ind] === column) {
+        secTblVal.splice(ind, 1);
+        break;
+      }
+    }
+    console.log('isPrimary', isPrimary, 'deleted', tableColMap);
+  }
+  togglePrimaryColumns(_event, column) {
+    const target = _event.target;
+    if (target.checked) {
+      this.selectedPrimCol.set(target.id, true);
+      this.addToTblColMap(column, true);
+    } else {
+      this.selectedPrimCol.delete(target.id);
+      this.removeFromTblColMap(column, true);
+    }
+    this.enableDisableNextBtn();
+  }
+  toggleSecTbl(_event) {
+    const target = _event.target;
+    if (target.checked) {
+      this.selectedSecTbl.set(target.id, true);
+      this.getColumnsByTableName(target.id, false);
+    } else {
+      this.selectedSecTbl.delete(target.id);
+    }
+    this.enableDisableNextBtn();
+  }
+  toggleSecColumns(_event, column) {
+    const target = _event.target;
+    if (target.checked) {
+      column.selected = true;
+      this.selectedSecCol.set(column.column_name, true);
+      this.addToTblColMap(column, false);
+    } else {
+      this.selectedSecCol.delete(column.column_name);
+      this.removeFromTblColMap(column, false);
+    }
+    this.enableDisableNextBtn();
+  }
+  generateSecTblArray() {
+    this.tableList.forEach((el) => {
+      // console.log(el, this.selectedPrimTbl, el !== this.selectedPrimTbl);
+      if (el !== this.selectedPrimTbl) {
+        this.secTblArray.push(el);
+      }
+    });
+    this.enableNextBtn = this.selectedSecTbl.size > 0;
+  }
+  getCurrentStep() {
+    return document.querySelector('#dataAModal-carousel .item.active').getAttribute('step');
+  }
+  enableDisableNextBtn() {
+    const currentStep = this.getCurrentStep();
+    switch (currentStep) {
+      case '0':
+        this.enableNextBtn = this.selectedPrimCol.size > 0;
+        break;
+      case '1':
+        this.enableNextBtn = this.selectedSecColMap.size > 0;
+        break;
+      case '2':
+        this.enableNextBtn = this.selectedSecCol.size > 0;
+        break;
+      // case '3':
+      //   this.enableNextBtn = this.selectedPrimCol.size > 0;
+      //   break;
+      default:
+        break;
+    }
+  }
+  // carousel handling codes
+  addClass(elementId, classSelector) {
+    document.getElementById(elementId).classList.add(classSelector);
+  }
+  removeClass(elementId, classSelector) {
+    document.getElementById(elementId).classList.remove(classSelector);
+  }
+
+  prevStep(e) {
+    document.getElementById('prev-slide').click();
+    this.handleStepIindicator(false);
+  }
+
+  nextStep(e) {
+    document.getElementById('next-slide').click();
+    this.handleStepIindicator(true);
+  }
+
+  handleStepIindicator(isNext) {
+    const slideNo = this.getCurrentStep();
+    const progressSelector = 'progress-bar';
+    // console.log('slideNo', slideNo, document.getElementById(progressSelector));
+    switch (slideNo) {
+      case '0':
+        // this.removeClass(progressSelector, 'width-5-pc');
+        // this.removeClass(progressSelector, 'width-5-25-pc-rev');
+        // this.addClass(progressSelector, 'width-5-25-pc');
+        // // this.addClass('cancel-btn', 'hide');
+        this.removeClass(progressSelector, 'width-5-pc');
+        this.removeClass(progressSelector, 'width-33-pc-rev');
+        this.addClass(progressSelector, 'width-33-pc');
+        this.removeClass('prev-btn', 'hide');
+        this.generateSecTblArray();
+        break;
+      case '1':
+        // this.removeClass(progressSelector, 'width-5-25-pc');
+        this.removeClass(progressSelector, 'width-33-pc');
+        if (isNext) {
+          // this.addClass(progressSelector, 'width-25-50-pc');
+          // this.removeClass(progressSelector, 'width-25-50-pc-rev');
+          this.addClass(progressSelector, 'width-66-pc');
+          this.removeClass(progressSelector, 'width-66-pc-rev');
+          this.removeClass('analyse-btn', 'hide');
+          this.addClass('next-btn', 'hide');
+        } else {
+          // this.removeClass(progressSelector, 'width-25-50-pc-rev');
+          // this.addClass(progressSelector, 'width-5-25-pc-rev');
+          this.removeClass(progressSelector, 'width-66-pc-rev');
+          this.addClass(progressSelector, 'width-33-pc-rev');
+          // this.removeClass('cancel-btn', 'hide');
+          this.removeClass('next-btn', 'hide');
+          this.addClass('prev-btn', 'hide');
+        }
+        this.enableDisableNextBtn();
+        break;
+      case '2':
+        // this.removeClass(progressSelector, 'width-25-50-pc');
+        // this.removeClass(progressSelector, 'width-50-75-pc-rev');
+        this.removeClass(progressSelector, 'width-66-pc');
+        // this.removeClass(progressSelector, 'width-100-pc-rev');
+        if (isNext) {
+          // this.removeClass(progressSelector, 'width-5-25-pc-rev');
+          // this.addClass(progressSelector, 'width-50-75-pc');
+          // this.removeClass(progressSelector, 'width-33-pc-rev');
+        } else {
+          // this.removeClass(progressSelector, 'width-75-100-pc-rev');
+          // this.addClass(progressSelector, 'width-25-50-pc-rev');
+          // this.addClass(progressSelector, 'width-100-pc-rev');
+          this.addClass(progressSelector, 'width-66-pc-rev');
+          this.addClass('analyse-btn', 'hide');
+          this.removeClass('next-btn', 'hide');
+        }
+        break;
+      // case '3':
+      //   // this.removeClass(progressSelector, 'width-50-75-pc');
+      //   this.removeClass(progressSelector, 'width-100-pc');
+      //   if (isNext) {
+      //     // this.addClass(progressSelector, 'width-75-100-pc');
+      //     // this.addClass(progressSelector, 'width-75-100-pc');
+      //     // this.removeClass('analyse-btn', 'hide');
+      //     // this.addClass('next-btn', 'hide');
+      //     // this.addClass('prev-btn', 'hide');
+      //   } else {
+      //     this.addClass(progressSelector, 'width-100-pc-rev');
+      //   }
+      //   break;
+      default:
+        break;
+    }
+  }
+  dataAnalyse() {
+    const progressSelector = 'progress-bar';
+    this.addClass(progressSelector, 'width-100-pc');
+    this.addClass('prev-btn', 'hide');
+  }
 }
--- src/app/table-list/table-list.service.ts
+++ src/app/table-list/table-list.service.ts
@@ -15,16 +15,11 @@ export class TableListService {
   accessToken: string;
   jwtHelper: JwtHelper = new JwtHelper();
   private serviceActionType: string;
-  tableListUrl = environment.apiUrl + '/metadata/tableList';
+  tableListUrl = environment.apiUrl + '/tables';
   relationTableListUrl = environment.apiUrl + '/metadata/';
-  private headers;
+  columnUrl = environment.apiUrl + '/tables/meta/info?tableName=';
   constructor(private http: HttpClient,
     private userinfoService: UserinfoService) {
-    this.headers = new HttpHeaders(
-      {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + this.userinfoService.getAuthKey()
-      });
   }
   getTableList(): Observable<string[]> {
     return this.http.get<string[]>(this.tableListUrl, { headers: this.userinfoService.getHeaders() })
@@ -40,6 +35,13 @@ export class TableListService {
       .pipe(catchError(this.handleError('relationtable-getListOfRelationTable()', []))
       );
   }
+  getColumnsByTableName(tableName) {
+    console.log('Fetching columns for:', tableName);
+    const url = this.columnUrl + tableName;
+    return this.http.get<any[]>(url, {headers: this.userinfoService.getHeaders()})
+    .map(this.extractTablesMeta)
+    .pipe(catchError(this.handleError('getColumnsByTableName()', [])));
+  }
 
   private extractTables(res: any) {
     const data = res.data.tables;
@@ -50,6 +52,11 @@ export class TableListService {
     return data || [];
   }
 
+  private extractTablesMeta(res: any) {
+    console.log(res);
+    const data = res.data.tables_meta.table_columns;
+    return data || [];
+  }
   setServiceActionType(serviceActionType: string) {
     this.serviceActionType = serviceActionType;
   }
@@ -77,6 +84,4 @@ export class TableListService {
   private log(message: string) {
     console.log(message);
   }
-
-
 }
