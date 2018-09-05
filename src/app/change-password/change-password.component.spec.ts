0f7237c44ef8e2790425efe3b99a3ccb3fa8a8a6
Shammi Hans <shammihans1@gmail.com>
2018-09-05 18:29:43 +0530

    imported required modules and providers in change-password spec file to run test-case properly

--- src/app/change-password/change-password.component.spec.ts
+++ src/app/change-password/change-password.component.spec.ts
@@ -1,14 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { ChangePasswordComponent } from './change-password.component';
+import { ChangePasswordService } from './change-password.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { JwtHelper } from 'angular2-jwt';
 
-xdescribe('ChangePasswordComponent', () => {
+describe('ChangePasswordComponent', () => {
   let component: ChangePasswordComponent;
   let fixture: ComponentFixture<ChangePasswordComponent>;
+  const formBuilder: FormBuilder = new FormBuilder();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChangePasswordComponent ]
+      declarations: [ ChangePasswordComponent ],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        // reference the new instance of formBuilder from above
+        ChangePasswordService,
+        JwtHelper,
+        { provide: FormBuilder, useValue: formBuilder }
+      ]
     })
     .compileComponents();
   }));
@@ -16,6 +32,10 @@ xdescribe('ChangePasswordComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ChangePasswordComponent);
     component = fixture.componentInstance;
+    component.changePasswordForm = formBuilder.group({
+      firstName: null,
+      lastName: null
+    });
     fixture.detectChanges();
   });
 
