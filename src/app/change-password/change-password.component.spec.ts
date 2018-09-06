985aaebf1f9a8c4d0fbac7683228a951d2c20261
Shammi Hans <shammihans1@gmail.com>
2018-09-06 17:44:26 +0530

    test cases for ChangePasswordService injection & change password success when changeUserPassword()

--- src/app/change-password/change-password.component.spec.ts
+++ src/app/change-password/change-password.component.spec.ts
@@ -1,16 +1,30 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { ChangePasswordComponent } from './change-password.component';
 import { ChangePasswordService } from './change-password.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { JwtHelper } from 'angular2-jwt';
+import { Observable } from 'rxjs/Observable';
 
 describe('ChangePasswordComponent', () => {
   let component: ChangePasswordComponent;
   let fixture: ComponentFixture<ChangePasswordComponent>;
   const formBuilder: FormBuilder = new FormBuilder();
+  let testBedService: any;
+  const getSimpleObservable = function(data) {
+    return new Observable<any>((observer) => {
+      observer.next(data); // observable execution
+      observer.complete();
+    });
+  };
 
+  const disposeMe = new Map();
+  const getJobList = function (data): Observable<any> {
+    const pvtObservable = getSimpleObservable(data);
+    disposeMe.set('getJobList', pvtObservable.subscribe());
+    return pvtObservable;
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ChangePasswordComponent ],
@@ -33,13 +47,22 @@ describe('ChangePasswordComponent', () => {
     fixture = TestBed.createComponent(ChangePasswordComponent);
     component = fixture.componentInstance;
     component.changePasswordForm = formBuilder.group({
-      firstName: null,
-      lastName: null
+      oldPassword: null,
+      newPassword: null,
+      confirmPassword: null
     });
     fixture.detectChanges();
+    testBedService = TestBed.get(ChangePasswordService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('ChangePasswordService injected via inject(...) and TestBed.get(...) should be the same instance',
+    inject([ChangePasswordService], (injectService: ChangePasswordService) => {
+      expect(injectService).toBe(testBedService);
+    })
+  );
+
 });
