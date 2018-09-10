421d4cb44b2cb881c9c9eb5522651c67d5bbaaae
Shammi Hans <shammihans1@gmail.com>
2018-09-10 20:26:09 +0530

    test - case for should userId be same as passed in changeGlobalRoleDetails service

--- src/app/change-user-role/change-user-role.service.spec.ts
+++ src/app/change-user-role/change-user-role.service.spec.ts
@@ -1,12 +1,14 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, fakeAsync, flushMicrotasks } from '@angular/core/testing';
 
 import { ChangeUserRoleService } from './change-user-role.service';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { MockBackend } from '@angular/http/testing';
-import { XHRBackend } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { XHRBackend, Http, ResponseOptions, Response } from '@angular/http';
 
 describe('ChangeUserRoleService', () => {
+  let backend: MockBackend;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -20,9 +22,31 @@ describe('ChangeUserRoleService', () => {
         HttpClientModule
       ]
     });
+    backend = TestBed.get(MockBackend);
+    spyOn(Http.prototype, 'request').and.callThrough();
   });
+  afterEach(() => backend.verifyNoPendingRequests());
 
   it('should be created', inject([ChangeUserRoleService], (service: ChangeUserRoleService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should userId be same as passed in changeGlobalRoleDetails service', fakeAsync(inject([ ChangeUserRoleService, MockBackend ],
+    (service: ChangeUserRoleService, mockBackend: MockBackend) => {
+      // prepare fake response from `MockBackend`
+      mockBackend.connections.subscribe((c: MockConnection) => {
+        // respond to `ChangePasswordService` with a 200 Ok
+        c.mockRespond(new Response(<ResponseOptions>{
+          body: '',
+          status: 200
+        }));
+      });
+      const userId = '5ac5d7af2e6c990861830974';
+      const globalId = '';
+      expect(service.passedUserId).toBeUndefined();
+      // dispatch the http request
+      service.changeGlobalRoleDetails(userId, globalId);
+      expect(service.passedUserId).toEqual(userId);
+      flushMicrotasks();
+  })));
 });
