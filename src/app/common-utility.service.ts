553dbaeb7b1531dd349c0af4285c6a77ecd4668e
Shammi Hans <shammihans1@gmail.com>
2018-09-25 12:36:43 +0530

    toggleCards common function to be used in different components

--- src/app/common-utility.service.ts
+++ src/app/common-utility.service.ts
@@ -11,37 +11,46 @@ export class CommonUtilityService {
     // private http: HttpClient
   ) { }
 
-    getDisplayTime(timeInteger) {
-        const dateTimeString = new Date(timeInteger).toString();
-        const dateTimeArray = dateTimeString.split(' ');
-        const month = dateTimeArray[1];
-        const day = dateTimeArray[2];
-        const year =  dateTimeArray[3];
-        const mmhhss = dateTimeArray[4];
-        const hrs = (dateTimeArray[4]).split(':')[0];
-        const isNoon = (Number(hrs) < 12);
-        const ampm = isNoon ? ' AM' : ' PM';
-        const mediumFormat = month + ' ' + day + ', ' + year + ', ' + mmhhss + ampm;
-        return mediumFormat;
+  getDisplayTime(timeInteger) {
+      const dateTimeString = new Date(timeInteger).toString();
+      const dateTimeArray = dateTimeString.split(' ');
+      const month = dateTimeArray[1];
+      const day = dateTimeArray[2];
+      const year =  dateTimeArray[3];
+      const mmhhss = dateTimeArray[4];
+      const hrs = (dateTimeArray[4]).split(':')[0];
+      const isNoon = (Number(hrs) < 12);
+      const ampm = isNoon ? ' AM' : ' PM';
+      const mediumFormat = month + ' ' + day + ', ' + year + ', ' + mmhhss + ampm;
+      return mediumFormat;
+  }
+  groupOutArray(list, groupNumber) {
+    let index = 0;
+    let array = [];
+    let groupedArray = [];
+    if (groupNumber > 1) {
+      list.forEach(element => {
+        if (index === groupNumber) {
+          groupedArray.push(array);
+          index = 0;
+          array = [];
+        }
+        array.push(element);
+        index++;
+      });
+      groupedArray.push(array);
+    } else {
+      groupedArray = list;
     }
-    groupOutArray(list, groupNumber) {
-      let index = 0;
-      let array = [];
-      let groupedArray = [];
-      if (groupNumber > 1) {
-        list.forEach(element => {
-          if (index === groupNumber) {
-            groupedArray.push(array);
-            index = 0;
-            array = [];
-          }
-          array.push(element);
-          index++;
-        });
-        groupedArray.push(array);
-      } else {
-        groupedArray = list;
-      }
-      return groupedArray;
+    return groupedArray;
+  }
+  toggleCard(cardId, toShow, _event) {
+    _event.stopPropagation();
+    const card = document.getElementById(cardId);
+    if (toShow) {
+      card.classList.add('reveal');
+    } else {
+      card.classList.remove('reveal');
     }
+  }
 }
