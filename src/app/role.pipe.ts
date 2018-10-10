1d642622f03a594707a88fae4dc396995a07514c
SatheeshKumarS26 <43089423+SatheeshKumarS26@users.noreply.github.com>
2018-10-10 15:56:57 +0530

    user role changes

--- src/app/role.pipe.ts
+++ src/app/role.pipe.ts
@@ -6,11 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class RolePipe implements PipeTransform {
   transform(value: any[]): any {
-    const filte = [ 'User', 'admin' ];
+    const filte = [ 'ROLE_ADMIN', 'ROLE_MEMBER' ];
     // tslint:disable:prefer-const
     let value1: any[] = [];
     for (let i of value) {
-      if (filte.includes(i.name)) {
+      if (filte.includes(i.globalRoles[0].roleName)) {
         value1.push(i);
       }
     }
