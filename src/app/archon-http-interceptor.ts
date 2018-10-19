9d78ddfb1dbffe34b5b12bbf3f0b87d61ca0478e
Shammi Hans <shammihans1@gmail.com>
2018-10-19 17:20:33 +0530

    added interceptor, redirects to login page and after successful login redirect to same old url, tested successfully

---
+++ src/app/archon-http-interceptor.ts
@@ -0,0 +1,47 @@
+import { HttpInterceptor, HttpRequest, HttpResponse, HttpErrorResponse, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { tap, catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { _throw } from 'rxjs/observable/throw';
+import { ArchonResponse } from './archon-response';
+import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { UserinfoService } from './userinfo.service';
+
+@Injectable()
+export class ArchonHttpInterceptor implements HttpInterceptor {
+    constructor(
+        private userinfoService: UserinfoService
+    ) {}
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    /*
+        // add a custom header
+        const customReq = request.clone({
+            headers: request.headers.set('app-language', 'it')
+        });
+        // pass on the modified request object
+        return next.handle(customReq);
+    */
+      return next
+        .handle(request)
+        .pipe(
+            tap((ev: any) => {
+                // console.log('ev', ev);
+            }),
+            catchError(response => {
+                this.redirectHandler(response);
+                return _throw(response);
+            })
+        );
+    }
+    redirectHandler(response: ArchonResponse) {
+        // console.log('Processing http error', response);
+        if (response) {
+            if (response.error) {
+                console.log(`Http error message "${response.error.errorMessage}"`);
+            }
+            if (response.status === 401) {
+                this.userinfoService.redirectOnSessionTimedOut();
+            }
+        }
+    }
+}
