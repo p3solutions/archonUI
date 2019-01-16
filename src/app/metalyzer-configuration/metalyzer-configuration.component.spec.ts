3e5d280709eb87c8fd9edba22aaefcfe9acae6b3
yowanrajan <43135572+yowanrajan@users.noreply.github.com>
2019-01-16 15:56:03 +0530

    fix: metalyzer configuration test cases

--- src/app/metalyzer-configuration/metalyzer-configuration.component.spec.ts
+++ src/app/metalyzer-configuration/metalyzer-configuration.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MetalyzerConfigurationComponent } from './metalyzer-configuration.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MetalyzerHeaderService } from '../metalyzer-header/metalyzer-header.service';
+import { HttpClientModule } from '@angular/common/http';
+import { UserinfoService } from '../userinfo.service';
+import { TableListService } from '../table-list/table-list.service';
 
-xdescribe('MetalyzerConfigurationComponent', () => {
+describe('MetalyzerConfigurationComponent', () => {
   let component: MetalyzerConfigurationComponent;
   let fixture: ComponentFixture<MetalyzerConfigurationComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MetalyzerConfigurationComponent ]
+      declarations: [ MetalyzerConfigurationComponent ],
+      imports: [RouterTestingModule, HttpClientModule],
+      providers: [MetalyzerHeaderService, UserinfoService, TableListService]
     })
     .compileComponents();
   }));
