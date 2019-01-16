311ead9c5694a03282c0e79df84e4d3dad26f915
yowanrajan <43135572+yowanrajan@users.noreply.github.com>
2019-01-16 14:17:09 +0530

    fix: database list test cases

--- src/app/database-list/database-list.component.spec.ts
+++ src/app/database-list/database-list.component.spec.ts
@@ -1,14 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DatabaseListComponent } from './database-list.component';
+import { DatabaseListService } from './database-list.service';
+import { HttpClientModule } from '@angular/common/http';
+import { UserinfoService } from '../userinfo.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DynamicLoaderService } from '../dynamic-loader.service';
+import { CommonUtilityService } from '../common-utility.service';
 
-xdescribe('DatabaseListComponent', () => {
+describe('DatabaseListComponent', () => {
   let component: DatabaseListComponent;
   let fixture: ComponentFixture<DatabaseListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DatabaseListComponent ]
+      declarations: [ DatabaseListComponent ],
+      imports: [HttpClientModule, RouterTestingModule],
+      providers: [DatabaseListService, UserinfoService, DynamicLoaderService, CommonUtilityService]
     })
     .compileComponents();
   }));
