22131070abe42a523c40345e035ae0591a6c7db2
yowanrajan <43135572+yowanrajan@users.noreply.github.com>
2019-01-22 17:12:32 +0530

    database view details https://app.asana.com/0/485886451522805/1001426392689210/f

--- src/app/database-list/database-list.service.ts
+++ src/app/database-list/database-list.service.ts
@@ -3,12 +3,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Headers, Response } from '@angular/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
-
-
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { UserinfoService } from '../userinfo.service';
 import { ConfiguredDB, DatabaseObject } from '../workspace-objects';
 import { environment } from '../../environments/environment';
+import { WorkspaceHeaderService } from '../workspace-header/workspace-header.service';
 
 @Injectable()
 export class DatabaseListService {
@@ -17,7 +16,10 @@ export class DatabaseListService {
   jwtHelper: JwtHelperService = new JwtHelperService();
 
   configDBListUrl = environment.apiUrl + '/dbs/configured';
+  private apiUrl = environment.apiUrl;
+  private getDBInfoUrl = this.apiUrl + '/dbs/configured/';
   constructor(private http: HttpClient,
+    private workspaceHeaderService: WorkspaceHeaderService,
     private userinfoService: UserinfoService) {
   }
   getListOfConfigDatabases(): Observable<ConfiguredDB[]> {
@@ -27,6 +29,20 @@ export class DatabaseListService {
         catchError(this.handleError('database-getList()', []))
       );
   }
+
+  getDBInfoByID(databaseId: string): Observable<ConfiguredDB> {
+    return this.http.get<ConfiguredDB>(this.getDBInfoUrl + databaseId, { headers: this.userinfoService.getHeaders() })
+    .pipe(
+      map(this.extractDataForDB),
+      catchError(this.handleError('getDBInfoByID', []))
+    );
+  }
+
+  private extractDataForDB(res: any) {
+    const body = res.data.configuredDatabases;
+    return body || [];
+  }
+
   private extractConfigDB(res: any) {
     const data = res.data.configuredDatabases;
     return data || [];
