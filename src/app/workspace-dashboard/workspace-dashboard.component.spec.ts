c055d8970e68c4a23a6527d3f0b571bc25893f9e
svudya <vudhyasaideepak@gmail.com>
2019-02-15 17:51:23 +0530

    latest changes

--- src/app/workspace-dashboard/workspace-dashboard.component.spec.ts
+++ src/app/workspace-dashboard/workspace-dashboard.component.spec.ts
@@ -40,7 +40,7 @@ describe('WorkspaceDashboardComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
