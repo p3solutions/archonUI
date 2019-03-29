3824033877c64b561787f5980416e0d538d663db
SatheeshKumar S <satheeshkumar.s@platform3solutions.com>
2019-03-29 19:58:13 +0530

    upto search

---
+++ src/app/status-screen/statusdatasource.ts
@@ -0,0 +1,39 @@
+import { DataSource } from '@angular/cdk/table';
+import { StatusService } from './status.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export class StatusDataSource implements DataSource<any> {
+
+    totalScreen: number;
+    private adhocSubject = new BehaviorSubject<any>([]);
+    searchArray = [];
+
+    constructor(private statusService: StatusService) { }
+
+    connect(): Observable<any> {
+        return this.adhocSubject.asObservable();
+    }
+
+    disconnect(): void {
+        this.adhocSubject.complete();
+    }
+
+    getTable(selectedJobOrigin, selectedJobStatus, startIndex) {
+        this.statusService.getJobList(selectedJobOrigin, selectedJobStatus, startIndex).subscribe((result) => {
+            result.list.forEach((value, index) => {
+                value.position = index + 1;
+            });
+            this.totalScreen = result.totalScreen;
+            this.adhocSubject.next(result.list);
+        });
+    }
+
+    filter(value) {
+        return this.adhocSubject.subscribe(result => {
+            this.searchArray = result;
+            this.searchArray.filter = value;
+            this.adhocSubject.next(this.searchArray);
+        });
+    }
+
+}
