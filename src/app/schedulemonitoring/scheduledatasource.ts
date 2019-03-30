51322539e5a3f8a3b9fbdc841a20ee87b74959ba
yowanrajan <43135572+yowanrajan@users.noreply.github.com>
2019-03-30 16:18:03 +0530

    schudule monitor anguler table

---
+++ src/app/schedulemonitoring/scheduledatasource.ts
@@ -0,0 +1,41 @@
+import { DataSource } from '@angular/cdk/table';
+import { ScheduleMonitoringService } from './schedule-monitoring.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export class ScheduleDataSource implements DataSource<any> {
+
+    totalScreen: number;
+    private adhocSubject = new BehaviorSubject<any>([]);
+    searchArray = [];
+
+    constructor(private service: ScheduleMonitoringService) { }
+
+    connect(): Observable<any> {
+        return this.adhocSubject.asObservable();
+    }
+
+    disconnect(): void {
+        this.adhocSubject.complete();
+    }
+
+    getTable(selectedTool, selectedJobStatus, startIndex) {
+        this.service.getJobStatuses(selectedTool, selectedJobStatus, startIndex).subscribe((result) => {
+            console.log(result, 'ss');
+            result.forEach((value, index) => {
+                value.position = index + 1;
+            });
+           // this.totalScreen = result.totalScreen;
+            this.adhocSubject.next(result);
+        });
+    }
+
+    // filter(value) {
+    //     this.adhocSubject.subscribe(result => {
+    //         this.searchArray = result;
+    //         this.searchArray.filter = value;
+    //     });
+    //     this.adhocSubject.next(this.searchArray);
+    //     return this.adhocSubject;
+    // }
+
+}
--- src/app/schedulemonitoring/schedulemonitoring.component.ts
+++ src/app/schedulemonitoring/schedulemonitoring.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, OnDestroy, AfterViewInit, Renderer, TemplateRef, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { ScheduleMonitoringService } from './schedule-monitoring.service';
+import { MatPaginator, MatSort } from '@angular/material';
+import { merge, fromEvent } from 'rxjs';
+import { tap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { ScheduleDataSource } from './scheduledatasource';
 
 @Component({
   selector: 'app-schedulemonitoring',
@@ -8,10 +12,8 @@ import { ScheduleMonitoringService } from './schedule-monitoring.service';
   styleUrls: ['./schedulemonitoring.component.css']
 })
 export class SchedulemonitoringComponent implements OnInit, AfterViewInit {
-
   loadStatus = true;
   isAvailable = false;
-  dtOptions: DataTables.Settings = {};
   output;
   Status = ['SCHEDULED', 'IN_PROGRESS', 'COMPLETED', 'FAILED' , 'USER_OR_ADMIN_STOPPED'];
   tools = ['RDBMS_EXTRACTION', 'ERT_EXTRACTION'];
@@ -25,40 +27,43 @@ export class SchedulemonitoringComponent implements OnInit, AfterViewInit {
   input;
   jobMessage;
   jobOutput: any;
+  displayedColumns: string[] = ['Job Type', 'Tool', 'User', 'Job Name',
+    'Job Runs', 'Schedule Time', 'Start Time', 'End Time', 'Last Run Time', 'Next Start Time', 'Status', 'Details', 'Stop'];
+  dataSource: ScheduleDataSource;
+  totalScreen = 0;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild('search') search: ElementRef;
 
   constructor(private router: Router, private renderer: Renderer, private service: ScheduleMonitoringService) {
   }
 
 
   ngOnInit(): void {
-    this.getStatus();
+    this.paginator.pageIndex = 0;
+    this.getStart();
   }
 
   ngAfterViewInit(): void {
-    const el: HTMLElement = this.button.nativeElement as HTMLElement;
-    this.renderer.listenGlobal('document', 'click', (event) => {
-    if (event.target.getAttribute('source') === 'Details') {
-      this.service.getDetails(event.target.getAttribute('id')).subscribe(result => {
-      this.common = result.common;
-      this.input = result.input;
-      this.jobMessage = result.message;
-      this.jobOutput = result.output;
-      console.log(result);
-      });
-      el.click();
-    } else if (event.target.getAttribute('source') === 'Stop') {
-      this.service.stopJob(event.target.getAttribute('id')).subscribe(result => {
-       if (result.success) {
-      this.updateSuccess = true;
-      this.message = result.data;
-      this.getStatus();
-       } else if (result.status === 500) {
-         this.updateNotif = true;
-         this.message = result.message;
-       }
-      });
-    }
-    });
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+
+    merge(this.sort.sortChange, this.paginator.page)
+      .pipe(
+        tap(() => this.loadPage())
+      )
+      .subscribe();
+  }
+
+  loadPage() {
+    this.dataSource.getTable(
+      this.selectedTool,
+       this.selectedJobStatus,
+      this.paginator.pageIndex + 1);
+  }
+
+  getStart() {
+    this.dataSource = new ScheduleDataSource(this.service);
+    this.dataSource.getTable(this.selectedTool, this.selectedJobStatus, this.paginator.pageIndex + 1);
   }
 
   selectTool(tool) {
@@ -73,96 +78,4 @@ export class SchedulemonitoringComponent implements OnInit, AfterViewInit {
     this.router.navigate(['workspace/workspace-dashboard/workspace-services']);
   }
 
-  getStatus() {
-    this.isAvailable = false;
-    this.service.getJobStatuses(this.selectedTool, this.selectedJobStatus).subscribe(x => {
-      this.output = x;
-      this.isAvailable = true;
-      this.renderTable();
-    });
-  }
-
-  renderTable() {
-    this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 10,
-      scrollX: true,
-      autoWidth: true,
-      data: this.output,
-      columns : [
-        {
-          title: 'Job Type',
-          data: 'jobType'
-        },
-        {
-          title: 'Tool',
-          data: 'tool'
-        },
-        {
-          title: 'User',
-          data: 'user'
-        },
-        {
-          title: 'Job Name',
-          data: 'jobName'
-        },
-        {
-          title: 'Job Runs',
-          data: 'jobRun'
-        },
-        {
-          title: 'Schedule Time',
-          data: 'scheduledDate'
-        },
-        {
-          title: 'Start Time',
-          data: 'scheduledDate'
-        },
-        {
-          title: 'End Time',
-          data: 'endDate'
-        },
-        {
-          title: 'Last Run Time',
-          data: 'lastRuntime'
-        },
-        {
-          title: 'Next Start Time',
-          data: 'nextStartTime'
-        },
-        {
-          title: 'Status',
-          render: function (data: any, type: any, full: any) {
-            if (full.status === 'COMPLETED') {
-            return '<a data-tooltip="Success"><i class="fa fa-thumbs-o-up fa-2x"></i></a>';
-            } else if (full.status === 'SCHEDULED') {
-              return '<a data-tooltip="Scheduled"><i class="fa fa-clock-o fa-2x"></i></a>';
-            } else if (full.status === 'IN_PROGRESS') {
-              return '<a data-tooltip="In_Progress"><i class="fa fa-spinner fa-2x"></i></a>';
-            } else if (full.status === 'FAILED') {
-              return '<a data-tooltip="Failed"><i class="fa fa-thumbs-o-down fa-2x"></i></a>';
-            } else if (full.status === 'USER_OR_ADMIN_STOPPED') {
-              return '<a data-tooltip="Stopped"><i class="fa fa-ban fa-2x"></i></a>';
-            }
-          }
-        },
-        {
-        title: 'Details',
-        render: function (data: any, type: any, full: any) {
-          return '<a data-tooltip="Details"><i class="fa fa-search fa-2x" id="' + full.scheduleId + '" source="Details"></i></a>';
-        }
-      },
-      {
-        title: 'Stop',
-        render: function (data: any, type: any, full: any) {
-          if (full.status !== 'USER_OR_ADMIN_STOPPED') {
-            return '<a data-tooltip="Stop"><i class="fa fa-stop-circle fa-2x" id="' + full.scheduleId + '" source="Stop"></i></a>';
-          } else {
-            return '<a data-tooltip="Stopped"><i class="fa fa-stop-circle fa-2x" style="color: grey"></i></a>';
-          }
-        }
-      }
-    ]
-    };
-  }
 }
