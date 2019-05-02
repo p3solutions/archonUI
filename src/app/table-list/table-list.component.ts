0793377a8511c9ca8186f5ce6a58a70080f66c6f
SatheeshKumar S <satheeshkumar.s@platform3solutions.com>
2019-05-02 19:04:08 +0530

    #429

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -81,6 +81,7 @@ export class TableListComponent implements OnInit {
   dynamicLoaderService: DynamicLoaderService;
   @ViewChild('storedprocView', { read: ViewContainerRef }) storedprocViewRef: ViewContainerRef;
 
+
   addDirectjoin: boolean;
   isTablelistAvailable: boolean;
   wsName: string;
@@ -239,6 +240,7 @@ export class TableListComponent implements OnInit {
   }
   // for selecting and mapping the checked values of table
   toggleColSelection(_event, isPrimary, column) {
+    console.log('in');
     const isChecked = _event.target.checked ? true : false;
     if (isPrimary) {
       for (let i = 0; i < this.primColArray.length; i++) {
@@ -691,5 +693,9 @@ export class TableListComponent implements OnInit {
       .subscribe(result => {
         this.downloadFilejson(result, result.type);
       });
+  }
+
+  selectAll() {
+      $('input:checkbox:enabled.m-r-10').click();
   }
 }
