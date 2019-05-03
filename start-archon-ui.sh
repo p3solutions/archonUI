c7836a02a42c82adf9a573223a49e1e597a45f38
svudya <vudhyasaideepak@gmail.com>
2019-05-03 14:50:42 +0530

    changed the variable name

--- start-archon-ui.sh
+++ start-archon-ui.sh
@@ -3,14 +3,14 @@
 # Frontend
 
 # Mandatory fields check
-if [[ -z "$HOSTNAME" -o -z "$PORT" ]]; then
+if [[ -z "$ARCHON_SERVER_HOST" ]] || [[ -z "$PORT" ]]; then
     echo "ERROR: HOSTNAME and PORT are mandatory environment variables"
     exit 1
 fi
 
 # Validating Archon Connectivity
 timer=0
-while ! nc -z $HOSTNAME $PORT; do
+while ! nc -z $ARCHON_SERVER_HOST $PORT; do
     if [[ $timer -le 120 ]]; then
         echo "Validating Archon Backend Connectivity"
         sleep 3
@@ -22,7 +22,7 @@ while ! nc -z $HOSTNAME $PORT; do
 done
 echo "Archon Backend connection validated"
 
-echo '{"apiUrl":"http://'$HOSTNAME':'$PORT'/"}' > /dist/assets/app-config.json
+echo '{"apiUrl":"http://'$ARCHON_SERVER_HOST':'$PORT'/"}' > /dist/assets/app-config.json
 
 echo "Starting frontend"
 /usr/sbin/nginx -g "daemon off;"
