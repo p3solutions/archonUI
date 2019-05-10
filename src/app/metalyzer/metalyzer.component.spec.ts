c6631dcf7561963bc2b0a93e395a1114b0f98268
SatheeshKumar S <satheeshkumar.s@platform3solutions.com>
2019-05-10 20:26:54 +0530

    test case fix

--- src/app/metalyzer/metalyzer.component.spec.ts
+++ src/app/metalyzer/metalyzer.component.spec.ts
@@ -11,7 +11,7 @@ import { WorkspaceHeaderService } from '../workspace-header/workspace-header.ser
 import { NgxPaginationModule } from 'ngx-pagination';
 import { EnvironmentService } from '../environment/environment.service';
 import { MockEnvironmentService } from '../environment/mock-environment.service';
-import { MatFormFieldModule, MatSelectModule } from '@angular/material';
+import { MatFormFieldModule, MatSelectModule, MatTableModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -22,7 +22,7 @@ describe('MetalyzerComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ MetalyzerComponent, MetalyzerHeaderComponent ],
-      imports: [RouterTestingModule, HttpClientModule, NgxPaginationModule, MatFormFieldModule, FormsModule, MatSelectModule, BrowserAnimationsModule],
+      imports: [RouterTestingModule,MatTableModule, HttpClientModule, NgxPaginationModule, MatFormFieldModule, FormsModule, MatSelectModule, BrowserAnimationsModule],
       providers: [TableListService, UserinfoService, MetalyzerHeaderService, WorkspaceHeaderService, { provide: EnvironmentService, useClass: MockEnvironmentService }]
     })
     .compileComponents();
