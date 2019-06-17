aff5beb6cffa572de9fda598b6eb1fa77c8f704d
shreeshmishra28 <shreesh.m@platform3solutions.com>
2019-06-17 17:36:54 +0530

    ert-test-case-fix

--- src/app/ert-jobs-config/ert-jobs-config.component.spec.ts
+++ src/app/ert-jobs-config/ert-jobs-config.component.spec.ts
@@ -2,12 +2,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErtJobsConfigComponent } from './ert-jobs-config.component';
 import { ErtService } from '../ert-landing-page/ert.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { UserinfoService } from '../userinfo.service';
 import { EnvironmentService } from '../environment/environment.service';
 import { MockEnvironmentService } from '../environment/mock-environment.service';
+import { MatFormFieldModule, MatTooltipModule, MatCardModule, MatInputModule } from '@angular/material';
+import { WorkspaceHeaderService } from '../workspace-header/workspace-header.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ErtJobsConfigComponent', () => {
   let component: ErtJobsConfigComponent;
@@ -15,11 +18,12 @@ describe('ErtJobsConfigComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ErtJobsConfigComponent ],
-      imports: [FormsModule, RouterTestingModule, HttpClientModule],
-      providers: [ErtService, UserinfoService, { provide: EnvironmentService, useClass: MockEnvironmentService }],
+      declarations: [ErtJobsConfigComponent],
+      imports: [FormsModule, RouterTestingModule, ReactiveFormsModule, MatInputModule, BrowserAnimationsModule,
+        HttpClientModule, MatFormFieldModule, MatTooltipModule, MatCardModule],
+      providers: [ErtService, WorkspaceHeaderService, UserinfoService, { provide: EnvironmentService, useClass: MockEnvironmentService }],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
