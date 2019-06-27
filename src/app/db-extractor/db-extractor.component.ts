4b76b9e25469201ebda8bfb39965095172f0a1bf
yowanrajan <43135572+yowanrajan@users.noreply.github.com>
2019-06-27 18:56:10 +0530

    query editor added

--- src/app/db-extractor/db-extractor.component.ts
+++ src/app/db-extractor/db-extractor.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DbExtractorService } from './db-extractor.service';
 import { ProgressBarObj } from '../db-extractor';
+
 @Component({
   selector: 'app-db-extractor',
   templateUrl: './db-extractor.component.html',
@@ -11,6 +12,7 @@ export class DbExtractorComponent implements OnInit {
   progressBarObj: ProgressBarObj;
   zone: any;
   ExtractData = false;
+  text: any;
   constructor(public router: Router, private dbExtractorService: DbExtractorService) {
     this.dbExtractorService.setProcessDetailsObj(null);
     this.dbExtractorService.setProgressBarObj({ stepTwoProgBarValue: 0, stepThreeProgBarValue: 0 });
