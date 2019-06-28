1004947006a3ea0bc07a0e0d34e116141004f448
yowanrajan <43135572+yowanrajan@users.noreply.github.com>
2019-06-28 12:24:06 +0530

    rdbms db datails

--- src/app/db-extractor/db-extractor.component.ts
+++ src/app/db-extractor/db-extractor.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DbExtractorService } from './db-extractor.service';
 import { ProgressBarObj } from '../db-extractor';
+import { WorkspaceHeaderService } from '../workspace-header/workspace-header.service';
+import { ConfiguredDB } from '../workspace-objects';
 
 @Component({
   selector: 'app-db-extractor',
@@ -13,7 +15,10 @@ export class DbExtractorComponent implements OnInit {
   zone: any;
   ExtractData = false;
   text: any;
-  constructor(public router: Router, private dbExtractorService: DbExtractorService) {
+  workspaceName = '';
+  configuredDB = new ConfiguredDB();
+  constructor(public router: Router, private dbExtractorService: DbExtractorService,
+    private workspaceHeaderService: WorkspaceHeaderService) {
     this.dbExtractorService.setProcessDetailsObj(null);
     this.dbExtractorService.setProgressBarObj({ stepTwoProgBarValue: 0, stepThreeProgBarValue: 0 });
   }
@@ -22,6 +27,12 @@ export class DbExtractorComponent implements OnInit {
       this.progressBarObj = progressBarObj;
       // this.router.navigate(['/workspace/db-extractor/db-extractor-process']);
     });
+    this.workspaceName = this.workspaceHeaderService.getSelectedWorkspaceName();
+    this.dbExtractorService.getDBInfoByID(this.workspaceHeaderService.getDatabaseID()).subscribe(
+      (result) => {
+        this.configuredDB = result;
+      }
+    );
   }
 
   processChange (type) {
