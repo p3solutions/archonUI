1d9a270b0bf64e8ab7dea398a2ec7759ca5e4885
shreeshmishra28 <shreesh.m@platform3solutions.com>
2019-07-09 18:33:42 +0530

    test case fix

--- src/app/ert-clone-view/ert-clone-view.component.spec.ts
+++ src/app/ert-clone-view/ert-clone-view.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErtCloneViewComponent } from './ert-clone-view.component';
+import { SearchPipe } from '../search.pipe';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { UserinfoService } from '../userinfo.service';
+import { WorkspaceHeaderService } from '../workspace-header/workspace-header.service';
+import { ErtService } from '../ert-landing-page/ert.service';
+import { EnvironmentService } from '../environment/environment.service';
+import { MockEnvironmentService } from '../environment/mock-environment.service';
 
 describe('ErtCloneViewComponent', () => {
   let component: ErtCloneViewComponent;
@@ -8,9 +17,11 @@ describe('ErtCloneViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ErtCloneViewComponent ]
+      declarations: [ErtCloneViewComponent, SearchPipe],
+      imports: [FormsModule, RouterTestingModule, HttpClientModule],
+      providers: [UserinfoService, WorkspaceHeaderService, ErtService, { provide: EnvironmentService, useClass: MockEnvironmentService }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
