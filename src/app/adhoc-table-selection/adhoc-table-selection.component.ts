5e6c0139ff25f6da436b08eb0331c80a59417316
SatheeshKumar S <satheeshkumar.s@platform3solutions.com>
2019-07-09 15:46:08 +0530

    strength variation

--- src/app/adhoc-table-selection/adhoc-table-selection.component.ts
+++ src/app/adhoc-table-selection/adhoc-table-selection.component.ts
@@ -310,7 +310,7 @@ export class AdhocTableSelectionComponent implements OnInit {
       .style('opacity', 0);
 
     const simulation = d3.forceSimulation()
-      .force('link', d3.forceLink().id(function (d: any) { return d.id; }).distance(100).strength(1)) // distance & strength added
+      .force('link', d3.forceLink().id(function (d: any) { return d.id; }).distance(100).strength(0.5)) // distance & strength added
       .force('charge', d3.forceManyBody().distanceMax(300).strength(-1000)) // added min, stength default:-15
       .force('center', d3.forceCenter(width / 2, height / 4))
       // .force('collide', d3.forceCollide())
