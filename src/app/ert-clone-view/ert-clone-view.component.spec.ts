8ae96a5b8f63129eba7f79a23a9e347f8298b4de
shreeshmishra28 <shreesh.m@platform3solutions.com>
2019-07-10 11:02:08 +0530

    test case fix

--- src/app/ert-clone-view/ert-clone-view.component.spec.ts
+++ src/app/ert-clone-view/ert-clone-view.component.spec.ts
@@ -10,6 +10,8 @@ import { WorkspaceHeaderService } from '../workspace-header/workspace-header.ser
 import { ErtService } from '../ert-landing-page/ert.service';
 import { EnvironmentService } from '../environment/environment.service';
 import { MockEnvironmentService } from '../environment/mock-environment.service';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ErtCloneViewComponent', () => {
   let component: ErtCloneViewComponent;
@@ -18,7 +20,7 @@ describe('ErtCloneViewComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ErtCloneViewComponent, SearchPipe],
-      imports: [FormsModule, RouterTestingModule, HttpClientModule],
+      imports: [FormsModule, RouterTestingModule, HttpClientModule, MatFormFieldModule, MatInputModule, BrowserAnimationsModule],
       providers: [UserinfoService, WorkspaceHeaderService, ErtService, { provide: EnvironmentService, useClass: MockEnvironmentService }]
     })
       .compileComponents();
