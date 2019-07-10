6b616514964eb909c92649691f684992d6493a2b
SatheeshKumar S <satheeshkumar.s@platform3solutions.com>
2019-07-11 00:52:15 +0530

    captcha

--- src/app/signin-form/signin-form.component.ts
+++ src/app/signin-form/signin-form.component.ts
@@ -19,8 +19,11 @@ export class SigninFormComponent implements OnInit {
   inProgress = false;
   enableSignInBtn = false;
   workspaceUrl = '/workspace';
-  recaptchaModel = '';
-  count = 0;
+  // recaptchaModel = '';
+  count = 4;
+  code;
+  captcha;
+  valid;
 
   constructor(
     private signinService: SigninFormService,
@@ -31,12 +34,14 @@ export class SigninFormComponent implements OnInit {
   ngOnInit() {
     this.createForm();
     setTimeout(() => this.enableSignIn(), 3000);
+    this.Captcha();
   }
 
   createForm() {
     this.signInForm = new FormGroup({
       userId: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required])
+      password: new FormControl('', [Validators.required]),
+      captcha: new FormControl( '', [Validators.required])
     });
   }
 
@@ -71,9 +76,10 @@ export class SigninFormComponent implements OnInit {
     this.errorObject = null;
   }
   enableSignIn() {
+    this.ValidCaptcha();
     if (this.signInForm.value.userId && this.signInForm.value.password && this.count <= 3) {
       this.enableSignInBtn = true;
-    } else if (this.signInForm.value.userId && this.signInForm.value.password && this.recaptchaModel !== '' && this.count > 3) {
+    } else if (this.signInForm.value.userId && this.signInForm.value.password && this.valid === true && this.count > 3) {
       this.enableSignInBtn = true;
     } else {
       this.enableSignInBtn = false;
@@ -88,8 +94,46 @@ export class SigninFormComponent implements OnInit {
     this.router.navigateByUrl(redirectUrl);
   }
 
-  resolved(captchaResponse: string) {
-    this.recaptchaModel = captchaResponse;
-    this.enableSignIn();
+//   resolved(captchaResponse) {
+//     console.log(captchaResponse);
+//     this.recaptchaModel = captchaResponse;
+//     this.enableSignIn();
+// }
+
+
+Captcha() {
+  const alpha = new Array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+  'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+  let i, a , b, c , d , e ,f , g;
+  for (i = 0; i < 6; i++) {
+      a = alpha[Math.floor(Math.random() * alpha.length)];
+      b = alpha[Math.floor(Math.random() * alpha.length)];
+      c = alpha[Math.floor(Math.random() * alpha.length)];
+      d = alpha[Math.floor(Math.random() * alpha.length)];
+      e = alpha[Math.floor(Math.random() * alpha.length)];
+      f = alpha[Math.floor(Math.random() * alpha.length)];
+      g = alpha[Math.floor(Math.random() * alpha.length)];
+      }
+  let code = a + ' ' + b + ' ' + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g;
+  // document.getElementById('mainCaptcha').innerHTML = code;
+  // (document.getElementById('mainCaptcha')as HTMLInputElement).value = code;
+  this.code = code;
+    }
+ValidCaptcha() {
+  const string1 = this.removeSpaces(this.code);
+  const string2 = this.removeSpaces(this.signInForm.value.captcha);
+  if (string1 === string2) {
+         this.valid = true;
+  } else {
+       this.valid = false;
+       }
 }
+
+removeSpaces(string) {
+  return string.split(' ').join('');
+}
+
+
 }
