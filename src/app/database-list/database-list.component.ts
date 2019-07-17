f973c97eabe7a891f5314339eae97f5a10e7e578
shreeshmishra28 <shreesh.m@platform3solutions.com>
2019-07-17 10:16:27 +0530

    sort db list

--- src/app/database-list/database-list.component.ts
+++ src/app/database-list/database-list.component.ts
@@ -111,7 +111,10 @@ export class DatabaseListComponent implements OnInit, OnDestroy {
     this.configDBListService.getListOfConfigDatabases().subscribe(result => {
       this.configDBListInfo = result;
       this.isProgress = false;
-      this.dbListActions = this.configDBListInfo;
+      this.dbListActions = this.configDBListInfo.filter(a => a.owner.id === this.userinfoId); // My DB
+      const otherUserDbList = this.configDBListInfo.filter(a => a.owner.id !== this.userinfoId); // My Other user DB
+      Array.prototype.push.apply(this.dbListActions, otherUserDbList); // Reorder
+      console.log(this.dbListActions);
       this.tempDbListActions = this.dbListActions.map(function (el) {
         const o = Object.assign({}, el);
         o.ownerId = el.owner.id;
