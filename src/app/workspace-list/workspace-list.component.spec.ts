d4212099b8443cff0c49f0ba9380e31c8cb2e46a
yowanrajan <43135572+yowanrajan@users.noreply.github.com>
2019-07-29 11:35:07 +0530

    test case fix

--- src/app/workspace-list/workspace-list.component.spec.ts
+++ src/app/workspace-list/workspace-list.component.spec.ts
@@ -13,7 +13,7 @@ import { CommonUtilityService } from '../common-utility.service';
 import { WorkspaceHeaderService } from '../workspace-header/workspace-header.service';
 import { SearchPipe } from '../search.pipe';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule, MatCardModule, MatInputModule, MatProgressBarModule } from '@angular/material';
+import { MatFormFieldModule, MatCardModule, MatInputModule, MatProgressBarModule, MatTooltipModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { EnvironmentService } from '../environment/environment.service';
 import { MockEnvironmentService } from '../environment/mock-environment.service';
@@ -28,7 +28,7 @@ describe('WorkspaceListComponent', () => {
     TestBed.configureTestingModule({
       declarations: [WorkspaceListComponent, SearchPipe],
       imports: [HttpClientModule, RouterTestingModule, MatFormFieldModule,
-        MatCardModule, MatInputModule, BrowserAnimationsModule, FormsModule, MatProgressBarModule],
+        MatCardModule, MatInputModule, BrowserAnimationsModule, FormsModule, MatProgressBarModule, MatTooltipModule],
       providers: [WorkspaceListService, DynamicLoaderService, UserinfoService,
         WorkspaceHeaderService, CommonUtilityService, NgxSpinnerService, { provide: EnvironmentService, useClass: MockEnvironmentService }],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
