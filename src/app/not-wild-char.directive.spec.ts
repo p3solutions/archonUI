559c5cbb9c8f6e357b725846d23b985d206f6375
shreesh mishra <shreesh.m@platform3solutions.com>
2019-08-20 12:49:48 +0530

    added wild char directive

---
+++ src/app/not-wild-char.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NotWildCharDirective } from './not-wild-char.directive';
+
+describe('NotWildCharDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NotWildCharDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/not-wild-char.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appNotWildChar]'
+})
+export class NotWildCharDirective {
+
+  constructor() { }
+
+}
